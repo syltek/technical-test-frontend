d41e436b67203461b0edee43fc2906b1ae86b5ae
Souler <juanjoseherrero@playtomic.io>
2024-03-06 11:54:08 +0100

    chore: remove noise from the test failure outputs

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,6 +1,18 @@
 import { afterEach } from 'vitest'
-import { cleanup } from '@testing-library/react'
+import { configure, cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom/vitest'
+
+beforeAll(() => {
+  // Setup RTL so error messages don't print the whole DOM on failure
+  // see: https://github.com/testing-library/dom-testing-library/issues/773
+  configure({
+    getElementError(message) { 
+        const error = new Error(message ?? '') 
+        error.name = 'TestingLibraryElementError' 
+        return error
+      }
+  })
+})
 
 // runs a clean after each test case (e.g. clearing jsdom)
 afterEach(() => {
