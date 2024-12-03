bdf8b5ad2b06e05bfa714024ab4b7674ec5acd13
Souler <juanjoseherrero@playtomic.io>
2024-12-03 17:41:26 +0100

    feat: add prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,6 +9,7 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'plugin:react/jsx-runtime',
+    'prettier'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
---
+++ .prettierrc.cjs
@@ -0,0 +1,12 @@
+/**
+ * @see https://prettier.io/docs/en/configuration
+ * @type {import('prettier').Config}
+ */
+module.exports = {
+  printWidth: 110,
+  arrowParens: 'avoid',
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSameLine: true,
+}
